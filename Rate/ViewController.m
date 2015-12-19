8ce879ae65c7f4871c00cf236ec6fa708bbf8026
summer <summer.okay@gmail.com>
2015-12-19 13:31:28 +0800

    Add Coin Class

--- Rate/ViewController.m
+++ Rate/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+
+
+
 
 @interface ViewController ()
 
@@ -18,11 +22,19 @@
     [super viewDidLoad];
     
     NSLog(@"testing...");
+    
+    
+    //NSPredicate *predicate = nil;
+    
+    NSString *url = @"http://op.juhe.cn/onebox/exchange/query?key=1b6f12399105b694b1d2c845df3f3ca9";
+    
+    //[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString: [url UTF8String] urf parameters:nil error:nil
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
